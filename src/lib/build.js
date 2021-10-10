15b22462d2968b917321aeb37fa59322a62ecccf
Cain Atkinson <yellowsink@protonmail.com>
2021-10-10 20:39:33 +0100

    the imports n shit are no longer broken

--- src/lib/build.js
+++ src/lib/build.js
@@ -1,16 +1,14 @@
 const genProxyConf = function (prop, acc) {
-    if (prop == "data") return acc;
+  if (prop == "data") return acc;
 
-    if (prop.length > 1)
-      throw new Error(
-        "Hell 🚀 disallows using multiple characters in HellChars 🚀 in your HellScript 🚀 for performance 🚀 reasons. 🚀"
-      );
-  
-    return new Proxy({}, { get: (_, p) => genProxyConf(p, acc + prop) });
-  };
-  
-const hell = (callback) => callback(genProxyConf(undefined, "", "")).data;
+  if (prop.length > 1)
+    throw new Error(
+      "Hell 🚀 disallows using multiple characters in HellChars 🚀 in your HellScript 🚀 for performance 🚀 reasons. 🚀"
+    );
 
-export { 
-    hell as build
+  return new Proxy({}, { get: (_, p) => genProxyConf(p, acc + prop) });
 };
+
+const hell = (callback) => callback(genProxyConf("", "")).data;
+
+export { hell as build };
