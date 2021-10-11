302aaece23329c90f5314ab130687b7e99a5e086
Cain Atkinson <19270622+yellowsink@users.noreply.github.com>
2021-10-11 20:44:33 +0000

    Create discordbot.js

---
+++ examples/discordbot.js
@@ -0,0 +1,20 @@
+import hell from "https://cdn.skypack.dev/hell.js"
+import { startBot } from "https://deno.land/x/discordeno/mod.ts";
+
+// this is purely to save me from typing my token in Hell 🚀
+// this discord token is invalid so dont even try
+let token = "NTE4NzEyODQyNjAyNDE0MTAw.XAOe-g.BustHy8dCDn-ooxx_TkV6ipIOR8"
+
+eval(hell((_) =>
+
+    _.s.t.a.r.t.B.o.t["("]["{"]
+        .t.o.k.e.n[","]
+        .i.n.t.e.n.t.s[":"]["["]["'"].G.u.i.l.d.s["'"][","]["'"].G.u.i.l.d.M.e.s.s.a.g.e.s["'"]["]"][","]
+        .e.v.e.n.t.H.a.n.d.l.e.r.s[":"]["{"]
+            .r.e.a.d.y["("][")"]["{"]
+                .c.o.n.s.o.l.e["."].l.o.g["("]["'"].W.e.l.c.o.m.e[" "].t.o[" "].H.e.l.l["."].j.s["'"][")"]
+            ["}"]
+        ["}"]
+    ["}"][")"]
+
+));
