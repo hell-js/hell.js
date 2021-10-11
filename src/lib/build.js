dc1b3158e5f338a726f13416acd03313de54a9eb
creatable <creatableog@protonmail.com>
2021-10-11 02:39:00 -0400

    Add security:tm:

--- src/lib/build.js
+++ src/lib/build.js
@@ -1,14 +1,23 @@
-const genProxyConf = function (prop, acc) {
-  if (prop == "data") return acc;
+let identifier = (Math.random() + 1).toString(36).substring(3);
+
+const buildProxy = function (prop, acc) {
+  if (prop == identifier) return acc;
 
   if (prop.length > 1)
     throw new Error(
       "Hell 🚀 disallows using multiple characters in HellChars 🚀 in your HellScript 🚀 for performance 🚀 reasons. 🚀"
     );
 
-  return new Proxy({}, { get: (_, p) => genProxyConf(p, acc + prop) });
+  return new Proxy({}, { get: (_, p) => buildProxy(p, acc + prop) });
 };
 
-const hell = (callback) => callback(genProxyConf("", "")).data;
+function hell(callback) {
+  let proxy = callback(buildProxy("", ""));
+  if (Array.isArray(proxy)) {
+    return proxy.map((p) => p[identifier]).join("");
+  } else {
+    return proxy[identifier];
+  }
+}
 
 export { hell as build };
